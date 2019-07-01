734b9b54e28a78cbe5785f8f67dd5d4c565ae38f
叶纪坤 <yejikun@chuanglin.onaliyun.com>
2019-07-01 10:21:30 +0800

    修改路由配置

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,15 @@ const router = new Router({
 /**
  * 文章接口
  */
+router
 //创建文章
-router.post('/article/create',ArtileController.create);
+    .post('/article/create',ArtileController.create)
+//获取文章列表
+    .post('/article/list',ArtileController.list);
 
 //获取文章详情
 router.get('/article/:id',ArtileController.detail)
 
-//获取文章列表
-router.post('/article/list',ArtileController.list)
+
 
 module.exports = router
